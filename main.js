6c36066f9e0980b4bc5d1ddca06906386663e2c2
HZ <moneydreamman@gmail.com>
2025-06-07 22:21:18 +0800

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-function greetUser() {
-    const name = document.getElementById("nameInput").value.trim();
-
-    const greeting = name ? `你好，${name}` : "请输入一个名字！";
-
-    document.getElementById("greeting").textContent = greeting;
-}
